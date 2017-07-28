9d65aed1a73c0afc75a84613bde562ae94e67c84
DESKTOP-F3OSGPL\Magnus <kontakt@magnuslarsen.dk>
2017-07-28 15:21:06 +0200

    Changes to view of match creation

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,10 @@
 var TIMER_VALUE = 5000;
 
-var HOST = "http://139.59.211.36/rankyTest";
-// var HOST = "http://localhost:3010/rankyTest";
+//var HOST = "http://139.59.211.36/rankyTest";
+ var HOST = "http://localhost:3020/rankyTest";
 
 
-angular.module("players", ['ngRoute'])
+angular.module("players", ['ng','ngRoute'])
     .controller('playersCtrl', function ($http, $scope, $timeout) {
 
         $timeout(getPlayers, 0);
@@ -40,7 +40,6 @@ angular.module("players", ['ngRoute'])
     }
 
 
-
 }).controller('NewMatches', function ($http, $scope, $timeout, $location) {
 
     $timeout(getPlayers, 0);
@@ -49,7 +48,7 @@ angular.module("players", ['ngRoute'])
 
         $http({
             method: 'GET',
-            url: HOST +'/api/players'
+            url: HOST + '/api/players'
         }).then(function successCallback(res) {
             $scope.players = res.data;
             $timeout(getPlayers, 2000000);
@@ -59,13 +58,28 @@ angular.module("players", ['ngRoute'])
     }
 
     $scope.newMatch = function (match) {
-        $http.post(HOST + "/api/matches", match).then(
+
+        var newMatch = {
+            "team1":[
+                match.score1,
+                match.player1,
+                match.player2
+            ],
+            "team2":[
+                match.score2,
+                match.player3,
+                match.player4
+            ]
+
+        }
+
+        $http.post(HOST + "/api/matches", newMatch).then(
             function ok(res) {
-                alert("Match was succesfully created!")
+                alert("Match was successfully created!")
                 $location.path("/Ranking")
             },
             function error(res) {
-                console.log("Todo: handle error");
+                console.log(res);
             });
     };
 
@@ -83,18 +97,19 @@ angular.module("players", ['ngRoute'])
                 controller: "matchesCtrl"
             })
             .when("/NewMatch", {
-                templateUrl: "views/NewMatch.html"
+                templateUrl: "views/NewMatch.html",
+                controller: "NewMatches"
             })
             .otherwise({
                 redirectTo: "/Ranking"
             });
     }).filter('makeBold', function () {
 
-}).filter('range', function() {
-    return function(input, min, max) {
+}).filter('range', function () {
+    return function (input, min, max) {
         min = parseInt(min); //Make string input int
         max = parseInt(max);
-        for (var i=min; i<max; i++)
+        for (var i = min; i < max; i++)
             input.push(i);
         return input;
     };
